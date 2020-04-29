661c7606ed6e469cce2d237b5a6eca27b0c25ffe
Rakad <rakad.wise.man@gmail.com>
2020-04-29 04:30:29 +0300

    finished

--- main.js
+++ main.js
@@ -1,2 +1,61 @@
-//write your code here to make the tests pass
+class Document{
+    constructor(EmployeeName){
+        this.EmployeeName=EmployeeName
+    }
+}
 
+class Employee{
+    constructor(name){
+        this.name=name
+    }
+    work(Office){
+        const doc=new Document(this.name)
+        for(let i=0;i<10;i++){
+            Office.documents.push(doc)
+        }
+    }
+}
+
+class Manager{
+    constructor(name,employees){
+        this.employees=[]
+        this.name=name
+    }
+    hireEmployee(name){
+        const emp=new Employee(name)
+        this.employees.push(emp)
+    }
+    askEmployeesToWork(Office){
+        this.employees.forEach(emp=>emp.work(Office))
+    }
+}
+
+class Cleaner{
+    constructor(name){
+        this.name=name
+    }
+    clean(){
+        console.log("Clean");
+        
+    }
+}
+
+class Office{
+    constructor(documents,managers,cleaners){
+        this.documents=[]
+        this.managers=[]
+        this.cleaners=[]
+    }
+    hireCleaner(name){
+        const cln=new Cleaner(name)
+        this.cleaners.push(cln)
+    }
+    hireManager(name){
+        const mgr=new Manager(name)
+        this.managers.push(mgr)
+    }
+    startWorkDay(){
+        this.managers.forEach(mgr=>mgr.askEmployeesToWork(this))
+        this.cleaners.forEach(cln=>cln.clean())
+    }
+}
